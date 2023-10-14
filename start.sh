dc88ffff9d0f325d0abb1c37f8538acbe8a31975
vinaychauhan01 <147112817+vinaychauhan01@users.noreply.github.com>
2023-10-14 22:41:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Obitoautofilter
 fi
-cd /PiroAutoFilterBot
+cd /Obitoautofilter
 pip3 install -U -r requirements.txt
 echo "Starting PiroAutoFilterBot...."
 python3 bot.py
