9cc4f8b16d125632a7e28ccbca376b5d99bf6805
vinaychauhan01 <147112817+vinaychauhan01@users.noreply.github.com>
2023-10-14 22:41:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/ritheshrkrm/PiroAutoFilterBot.git /PiroAutoFilterBot
+  git clone https://github.com/vinaychauhan01/Obitoautofilter
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /PiroAutoFilterBot
+  git clone $UPSTREAM_REPO /Obitoautofilter
 fi
 cd /PiroAutoFilterBot
 pip3 install -U -r requirements.txt
